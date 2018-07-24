8d0a49688871c5f50c13d1173584a24a8c934d1f
Zachary Scherrer <zachary.a.scherrer@gmail.com>
2018-07-24 09:58:50 -0500

    Initial commit

---
+++ Superheroes/Models/Superhero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Superheroes.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+
+    }
+}
